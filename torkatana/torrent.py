2fa511be507deed4eda601db8cd9c3bc5f0f87eb
Yasith Piyarathne <yasithpiyarathne@gmail.com>
2024-07-05 21:38:27 +0530

    added all planned features from libtorrent

--- torkatana/torrent.py
+++ torkatana/torrent.py
@@ -5,7 +5,6 @@ import math
 import os
 
 
-
 class File(NamedTuple):
     index: int
     path: str
@@ -21,11 +20,17 @@ class TorrentSlice(NamedTuple):
     last_size: int
 
 
+class FileSlice(NamedTuple):
+    file_index: int
+    offset: int
+    size: int
+
+
 class Torrent:
     def __init__(self, torrnt_path: str | os.PathLike):
         self.__torrent_path = torrnt_path
         self.__torrent_info = tp.parse_torrent_file(torrnt_path)
-        
+
         files = list()
         pos_pointer = 0
         for file_index, file in enumerate(self.__torrent_info['info']['files']):
@@ -35,20 +40,21 @@ class Torrent:
             pos_pointer += size
         self.__files = tuple(files)
 
-        self.__torrent_info['info']['pieces'] = tuple(self.__torrent_info['info']['pieces'])
+        self.__torrent_info['info']['pieces'] = tuple(
+            self.__torrent_info['info']['pieces'])
 
     @property
     def name(self) -> str:
         return self.__torrent_info['info']['name']
-    
+
     @property
     def path(self):
         return self.__torrent_path
-    
+
     @property
     def numFiles(self) -> int:
         return len(self.files)
-    
+
     @property
     def pieceLength(self) -> int:
         return self.__torrent_info['info']['piece length']
@@ -56,45 +62,99 @@ class Torrent:
     @property
     def files(self) -> tuple[File, ...]:
         return self.__files
-    
+
     @property
     def totalSize(self) -> int:
-        return sum(map(lambda f:f.size, self.files))
-    
+        return sum(map(lambda f: f.size, self.files))
+
     @property
     def numPieces(self) -> int:
         return math.ceil(self.totalSize/self.pieceLength)
-    
+
     @property
     def pieceRange(self) -> range:
         return range(self.numPieces)
-    
+
     @property
     def fileRange(self) -> range:
         return range(self.numFiles)
-    
+
     @property
     def hashes(self) -> tuple[str, ...]:
         return self.__torrent_info['info']['pieces']
-    
-    def mapFile(self, file_index:int, file_offset:int=0, size:typing.Optional[int]=None) -> TorrentSlice:
+
+    def pieceSize(self, pieceIndex: int) -> int:
+        if pieceIndex not in self.pieceRange:
+            return 0
+        elif pieceIndex == self.numPieces-1:
+            return self.totalSize-(self.pieceLength*(self.numPieces-1))
+        else:
+            return self.pieceLength
+
+    def mapFile(self, file_index: int, file_offset: int = 0, size: typing.Optional[int] = None) -> TorrentSlice:
 
         if file_index not in self.fileRange:
             raise IndexError('file index out of range')
-        
+
         offset = file_offset + self.files[file_index].offset
 
-        if not size: size = self.files[file_index].size - file_offset
+        if not size:
+            size = self.files[file_index].size - file_offset
 
         if (offset + size) > self.totalSize:
             raise ValueError('selection out of torrent range')
-        
+
         piece_index = math.floor(offset/self.pieceLength)
         piece_offset = offset % self.pieceLength
         first_size = min(size, self.pieceLength - piece_offset)
         n_pieces = math.ceil((size - (piece_offset))/self.pieceLength) + 1
         jumps = n_pieces - 1
-        last_size = size - min(size, self.pieceLength - piece_offset) - jumps * self.pieceLength
+        last_size = size - min(size, self.pieceLength -
+                               piece_offset) - jumps * self.pieceLength
 
         return TorrentSlice(piece_index, piece_offset, first_size, jumps, last_size)
 
+    def mapBlock(self, piece_index: int, piece_offset: int = 0, size: typing.Optional[int] = None, dynamic_size: bool = True) -> tuple[FileSlice, ...]:
+        if piece_index not in self.pieceRange:
+            raise IndexError("piece index out of range")
+
+        # TORRENT_ASSERT_PRECOND(num_files() > 0);
+        # TORRENT_ASSERT_PRECOND(size >= 0);
+
+        slices = list()
+
+        # if (m_files.empty()) return ret;
+        # TORRENT_ASSERT(max_file_offset / m_piece_length > static_cast<int>(piece));
+
+        if not size:
+            size = self.pieceLength
+
+        # offset in the torrent
+        global_offset = piece_index * self.pieceLength + piece_offset
+
+        if not (global_offset <= self.totalSize - size):
+            if not dynamic_size:
+                raise ValueError("selection out of torrent range")
+            else:
+                size = self.totalSize - global_offset
+
+        for file in self.files[::-1]:
+            if file.offset <= global_offset:
+                pointer_file_index = file.index
+                break
+
+        file_offset = global_offset - self.files[pointer_file_index].offset
+
+        while (size > 0):
+            pointer_file = self.files[pointer_file_index]
+
+            # I don't understand why this condition is here
+            # if (file_offset < std::int64_t(file_iter->size))
+            file_size = min(pointer_file.size - file_offset, size)
+            fs = FileSlice(pointer_file.index, file_offset, file_size)
+            slices.append(fs)
+            file_offset = 0
+            size -= file_size
+            pointer_file_index = self.files[pointer_file_index+1].index
+
+        return tuple(slices)
