c307111c741364313cb793cb7510eb41cff024a8
Yasith Piyarathne <yasithpiyarathne@gmail.com>
2025-01-25 16:57:38 +0530

    block parsers and namers cannot determine the how many digits are in the block since the number of blocks is unknown (now blocks can have dynamic sizes)

--- torkatana/blocker.py
+++ torkatana/blocker.py
@@ -14,18 +14,32 @@ def getNBlocks(total_size: int, block_size: int):
     return math.ceil(total_size/block_size)
 
 
-def getBlockNamerAndParser(pattern: str, n_digits: int) -> tuple[Callable[[int], str], Callable[[str], int]]:
-    placeholder = r"\d" * n_digits
-    index_range = range(10**n_digits)
+# def getBlockNamerAndParser(pattern: str, n_digits: int) -> tuple[Callable[[int], str], Callable[[str], int]]:
+#     placeholder = r"\d" * n_digits
+#     index_range = range(10**n_digits)
+
+#     def namer(index):
+#         if index not in index_range:
+#             raise ValueError("index out of valid range")
+#         return pattern.format(index=format(index, f"0{n_digits}"))
+
+#     def parser(name):
+#         match = re.match(pattern.format(
+#             index=f"(?P<index>{placeholder})"), name)
+#         if not match:
+#             raise ValueError("string does not parse into a valid index")
+#         intstr = match.group('index')
+#         return int(intstr)
+
+#     return namer, parser
+def getBlockNamerAndParser(pattern: str) -> tuple[Callable[[int], str], Callable[[str], int]]:
 
     def namer(index):
-        if index not in index_range:
-            raise ValueError("index out of valid range")
-        return pattern.format(index=format(index, f"0{n_digits}"))
+        return pattern.format(index=str(index))
 
     def parser(name):
         match = re.match(pattern.format(
-            index=f"(?P<index>{placeholder})"), name)
+            index=r"(?P<index>\d*)"), name)
         if not match:
             raise ValueError("string does not parse into a valid index")
         intstr = match.group('index')
